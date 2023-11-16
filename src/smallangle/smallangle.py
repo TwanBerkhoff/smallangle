6295c24793ff4abbe9d4d9275275a6d3f2ca4faf
TwanBerkhoff <twanneman4@gmail.com>
2023-11-16 10:49:37 +0100

    sin

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,14 +3,27 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def cmd_group():
+    pass
 
+@click.option(
+    "-n",
+    "--number",
+    default=1,
+    help="Number of steps between 0 and 2pi",
+    show_default=True,  # show default in help
+)
+@cmd_group.command()
+@click.argument("number")
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
-
+@cmd_group.command()
+@click.argument("number")
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -19,4 +32,4 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
